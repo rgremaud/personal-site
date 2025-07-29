536721d568b0b65dabf42e5d453969757a13e762
Romain Gremaud <rgremau@gmail.com>
2025-07-29 16:29:37 -0400

    password change and reset

--- django_project/settings.py
+++ django_project/settings.py
@@ -138,3 +138,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 CRISPY_TEMPLATE_PACK = "bootstrap5"
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
